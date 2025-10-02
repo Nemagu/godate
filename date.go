a6c74b556e0e6a265775643c4caf59368da23c97
VladimirGerman <vladimir.german.and@yandex.ru>
2025-10-03 00:02:23 +0700

    continue

--- date.go
+++ date.go
@@ -1,23 +1,75 @@
 package godate
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/Nemagu/godate/gregorian"
 )
 
-type Date uint64
+type Date struct {
+	year  int
+	month time.Month
+	day   int
+}
 
 func NewDate(year int, month time.Month, day int) (Date, error) {
-	return Date(0), nil
+	if err := ValidateDate(year, month, day); err != nil {
+		return Date{}, err
+	}
+	return Date{
+		year:  year,
+		month: month,
+		day:   day,
+	}, nil
 }
 
-func Now() Date {
-	return Date(0)
+func Today() Date {
+	timeNow := time.Now()
+	return Date{
+		year:  timeNow.Year(),
+		month: timeNow.Month(),
+		day:   timeNow.Day(),
+	}
 }
 
-func ZeroDate() Date {
-	return Date(0)
+func FromTime(t time.Time) Date {
+	return Date{
+		year:  t.Year(),
+		month: t.Month(),
+		day:   t.Day(),
+	}
 }
 
-func validateDate(count uint64) error {
+func ValidateDate(year int, month time.Month, day int) error {
+	if year < 0 || year > 9999 {
+		return fmt.Errorf("invalid year: %d", year)
+	}
+	if month < 1 || month > 12 {
+		return fmt.Errorf("invalid month: %d", month)
+	}
+	if day < 1 || day > gregorian.DaysInMonth(year, month) {
+		return fmt.Errorf("invalid day: %d", day)
+	}
 	return nil
+}
+
+func (d Date) Year() int {
+	return d.year
+}
+
+func (d Date) Month() time.Month {
+	return d.month
+}
+
+func (d Date) Day() int {
+	return d.day
+}
+
+func (d Date) ToTime(loc *time.Location) time.Time {
+	return time.Date(d.year, d.month, d.day, 0, 0, 0, 0, loc)
+}
+
+func (d *Date) String() string {
+	return fmt.Sprintf("%04d-%02d-%02d", d.year, d.month, d.day)
 }
---
+++ gregorian/util.go
@@ -0,0 +1,37 @@
+package gregorian
+
+import "time"
+
+var daysInMonth = []int{
+	0,
+	31, //January
+	28,
+	31, // March
+	30,
+	31, // May
+	30,
+	31, // July
+	31,
+	30, // September
+	31,
+	30, // November
+	31,
+}
+
+func DaysInYear(year int) int {
+	if isLeap(year) {
+		return 366
+	}
+	return 365
+}
+
+func DaysInMonth(year int, month time.Month) int {
+	if month == time.February && isLeap(year) {
+		return 29
+	}
+	return daysInMonth[month]
+}
+
+func isLeap(year int) bool {
+	return year%400 == 0 || (year%4 == 0 && year%100 != 0)
+}
