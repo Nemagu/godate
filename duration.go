d6f37b9c1dd5879786a4daaffa14b6813e6de7ec
VladimirGerman <vladimir.german.and@yandex.ru>
2025-10-06 20:35:12 +0700

    make docs for duration.go file

--- duration.go
+++ duration.go
@@ -1,9 +1,11 @@
 package godate
 
+// A Duration represents the elapsed date between two instances as an int32 days count.
 type Duration int32
 
 const Day = Duration(1)
 
+// Days returns the duration as a integer number of days.
 func (d Duration) Days() int {
 	return int(d)
 }
