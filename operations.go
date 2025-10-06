2322a900b008c6f52ab7091045ee6095a383dd25
VladimirGerman <vladimir.german.and@yandex.ru>
2025-10-06 21:10:32 +0700

    make docs for operations.go file

--- operations.go
+++ operations.go
@@ -1,5 +1,9 @@
 package godate
 
+// Compare compares the date d with other.
+// If t is before other, it returns -1;
+// if t is after other, it returns +1;
+// if they're the same, it returns 0.
 func (d Date) Compare(other Date) int {
 	switch {
 	case d < other:
@@ -11,22 +15,27 @@ func (d Date) Compare(other Date) int {
 	}
 }
 
+// Equal reports whether d and other represent the same date.
 func (d Date) Equal(other Date) bool {
 	return d == other
 }
 
+// Before reports whether the date d is before other.
 func (d Date) Before(other Date) bool {
 	return d < other
 }
 
+// After reports whether the date d is after other.
 func (d Date) After(other Date) bool {
 	return d > other
 }
 
+// Sub returns the duration d-other.
 func (d Date) Sub(other Date) Duration {
 	return Duration(d) - Duration(other)
 }
 
+// Add returns the date d+other.
 func (d Date) Add(duration Duration) Date {
 	switch {
 	case duration == 0:
